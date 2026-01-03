25ba1cbc698e8bb2c0ce9279087357f88ca7afb6
szenesis <slypotter7@gmail.com>
2026-01-03 00:06:15 -0600

    Modifications

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 set -ouex pipefail
-
 systemctl enable systemd-timesyncd
 systemctl enable systemd-resolved.service
-
 # See https://github.com/CentOS/centos-bootc/issues/191
 mkdir -p /var/roothome
 
 #Make sure flatpak is active
 dnf5 install -y flatpak
-
-#Bazaar store
-flatpak install -y flathub io.github.kolunmi.Bazaar
-
-#Flatpak browser install
-flatpak install -y com.vivaldi.Vivaldi
-
-#Flatpak exention manager install
-
-
+#Flatpak browser and other necesary installs
+flatpak install -y \
+ com.vivaldi.Vivaldi \
+ com.mattjakeman.ExtensionManager
+#Remove and cleanup of flatpaks
+flatpak remove -y \
+ org.gnome.Extensions \
+ org.gnome.Contacts \
+ org.gnome.Maps \
+ org.gnome.Papers \
+ org.gnome.Connections
 # Install terminal software from fedora repos
 dnf5 install -y \
- sakura \
+ foot \
  fish \
  docker \
  fastfetch \
- fzf
-
+ fzf \
+ emacs
 #Remove software that is not needed for workflow.
 dnf5 remove -y \
  gnome-software \
@@ -36,8 +35,8 @@ dnf5 remove -y \
  yelp \
  htop \
  nvtop \
- firefox
-
+ firefox \
+ ptyxis
 #Remove old/retro Gnome extensions
 dnf5 remove -y \
  gnome-shell-extension-common \
@@ -49,23 +48,20 @@ dnf5 remove -y \
  gnome-shell-extension-appindicator \
  gnome-extensions-app \
  gnome-software-rpm-ostree \
- gnome-terminal \
  gnome-backgrounds \
- gnome-console \
- gnome-maps \
-
+ gnome-terminal \
 # Install VS Code
-tee /etc/yum.repos.d/vscode.repo <<'EOF'
-[code]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF
-sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/vscode.repo
-dnf -y install --enablerepo=code \
-    code
+#tee /etc/yum.repos.d/vscode.repo <<'EOF'
+#[code]
+#name=Visual Studio Code
+#baseurl=https://packages.microsoft.com/yumrepos/vscode
+#enabled=1
+#gpgcheck=1
+#gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+#EOF
+#sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/vscode.repo
+#dnf -y install --enablerepo=code \
+#    code
 
 
 systemctl preset systemd-resolved.service
