2e12d3309f40c12d2f4d3090829dba2d5a74c3aa
szenesis <slypotter7@gmail.com>
2026-09-11 21:19:14 -0600

    fixing bazzar default install

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -72,7 +72,7 @@ org.gnome.TextEditor
 org.fedoraproject.MediaWriter
 "
 # Default Bazzar Store Integration
-flatpak install flathub io.github.kolunmi.Bazaar
+flatpak install --system flathub io.github.kolunmi.Bazaar
 
 for app in $APPS; do
   if flatpak info --system "$app" >/dev/null 2>&1; then
